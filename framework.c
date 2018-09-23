e2b6f6240b25051c7a933974644da2ad09d5aa3b
agoston <agoston@kth.se>
2018-09-23 20:45:43 +0200

    bug fixes, time handling improvements

--- framework.c
+++ framework.c
@@ -1,11 +1,18 @@
+#include "framework.h"
 
 OnboardTC fifochannel sendTMQueue;
 
 task Initialization( void * vParameters) {
+	int16_t error = 0;
 	InitializationParameters * sParameters = (InitializationParameters *) vParameters;
-	printf("%lu\tID:0x%X\ttask started\n", gettime(ms), sParameters->taskID);
+	printf("%lu\tID:0x%X\ttask started\n", gettime(sec), sParameters->taskID);
 
 	numberOfTasks = sParameters->numberOfTasks;
+
+	if (numberOfTasks > MAX_NUMBER_OF_TASKS)
+		printf("number of tasks to be created (%u) larger than the maximum (%u)",
+		       numberOfTasks,
+		       MAX_NUMBER_OF_TASKS);
 
 	uint8_t numberOfInit = 0;
 	uint8_t numberOfGetTC = 0;
@@ -56,7 +63,17 @@ task Initialization( void * vParameters) {
 		}
 	}
 
-	NumberOfPayloadQueues = numberOfControlPL;
+	if (numberOfSubsystem > MAX_NUMBER_OF_SUBSYSTEMS)
+		printf("number of subsystems to be created (%u) is larger than the maximum (%u)",
+		       numberOfSubsystem,
+		       MAX_NUMBER_OF_SUBSYSTEMS);
+
+	if (numberOfControlPL > MAX_NUMBER_OF_PAYLOADS)
+		printf("number of payloads to be created (%u) is larger than the maximum (%u)",
+		       numberOfControlPL,
+		       MAX_NUMBER_OF_PAYLOADS);
+
+	numberOfPayloadQueues = numberOfControlPL;
 
 	int iQueue = 0;
 
@@ -91,109 +108,12 @@ task Initialization( void * vParameters) {
 	}
 
 
-	sParameters->initialization();
-	printf("%lu\tID:0x%X\ttask finished, deleting myself\n", gettime(ms), sParameters->taskID);
+	error = sParameters->initialization();
+	if (error) printf("error in initialization function: %d\n", error);
+	printf("%lu\tID:0x%X\ttask finished, deleting myself\n", gettime(sec), sParameters->taskID);
 
 	//TODO delete task here
 }
-
-
-task HandleCMD(void * vParameters) {
-	HandleCMDParameters * sParameters = (HandleCMDParameters *) vParameters;
-	long timeVal;
-	long delayUntilNextCMD;
-
-	OnboardTC tc;
-	OnboardTC *nextCmd;
-
-	printf("%lu\tID:0x%X\ttask started\n", gettime(ms), sParameters->taskID);
-
-	int i = 0;
-	while (1) {
-		printf("%lu\tID:0x%X\ttask running\n", gettime(ms), sParameters->taskID);
-
-		if (prioq->len > 0) {
-
-			nextCmd = top(prioq);
-
-			timeVal = gettime(ms);
-
-			if (nextCmd->time - timeVal > 0)
-				delayUntilNextCMD = nextCmd->time - timeVal;
-			else
-				delayUntilNextCMD = 0;
-
-
-			if (sParameters->period < delayUntilNextCMD ) {
-				sdelay(sParameters->period, ms);
-			}
-			else {
-				sdelay(delayUntilNextCMD);
-				pop(prioq, &tc);
-
-				if (tc.taskID == SendTMTaskID) {
-					cwrite(sendTMQueue, tc);
-				}
-				else {
-					for (i = 0; i < NumberOfPayloadQueues; i++) {
-						if (payloadQueues[i].taskID == tc.taskID)
-							break;
-					}
-					if (i==NumberOfPayloadQueues) 
-						printf("can't find queue for %u task\n",sParameters->taskID);
-					else 
-						cwrite(payloadQueues[i].queueHandle, tc);
-
-				}
-			}
-		}
-		else
-			sdelay(sParameters->period);
-	}
-}
-
-
-task Subsystem( void * vParameters) {
-
-	SubsystemParameters * sParameters = (SubsystemParameters *) vParameters;
-
-	spriority(sParameters->priority);
-
-	DynamicData dataFromSensors;
-	uint8_t dataFromSensorsBuffer[MAX_DATA_SIZE_SUBSYSTEM];
-	dataFromSensors.data = dataFromSensorsBuffer;
-	dataFromSensors.size = MAX_DATA_SIZE_SUBSYSTEM;
-
-	DynamicData dataToActuators;
-	uint8_t dataToActuatorsBuffer[MAX_DATA_SIZE_SUBSYSTEM];
-	dataToActuators.data = dataToActuatorsBuffer;
-	dataToActuators.size = MAX_DATA_SIZE_SUBSYSTEM;
-
-	uint16_t error = 0;
-
-	sParameters->initialization();
-
-	printf("%lu\tID:0x%X\ttask started\n", gettime(ms), sParameters->taskID);
-
-	while (1) {
-		printf("%lu\tID:0x%X\ttask running\n", gettime(ms), sParameters->taskID);
-
-		error = 0;
-
-		//read values from magnetometers
-		error = sParameters->receiveOnboard(&dataFromSensors);
-
-		//calculate
-		if (!error) error = sParameters->calculation(&dataFromSensors, &dataToActuators);
-
-		//send back results to subsystem
-		if (!error) error = sParameters->sendOnboard(&dataToActuators);
-
-		sdelay(sParameters->period, ms);
-	}
-
-}
-
 
 task GetTC(void * vParameters) {
 
@@ -201,7 +121,7 @@ task GetTC(void * vParameters) {
 
 	spriority(sParameters->priority);
 
-	uint16_t error = 0;
+	int16_t error = 0;
 
 	DynamicData dataFromCS;
 	uint8_t dataFromCSBuffer[MAX_DATA_SIZE_TC];
@@ -211,10 +131,10 @@ task GetTC(void * vParameters) {
 	OnboardTC receivedTC;
 	receivedTC.size = MAX_DATA_SIZE_TC;
 
-	printf("%lu\tID:0x%X\ttask started\n", gettime(ms), sParameters->taskID);
+	printf("%lu\tID:0x%X\ttask started\n", gettime(sec), sParameters->taskID);
 
 	while (1) {
-		printf("%lu\tID:0x%X\ttask running\n", gettime(ms), sParameters->taskID);
+		printf("%lu\tID:0x%X\ttask running\n", gettime(sec), sParameters->taskID);
 
 		error = sParameters->receiveOnboard(&dataFromCS);
 
@@ -228,10 +148,69 @@ task GetTC(void * vParameters) {
 }
 
 
+task HandleCMD(void * vParameters) {
+	HandleCMDParameters * sParameters = (HandleCMDParameters *) vParameters;
+	uint64_t timeValSec;
+	uint64_t timeVal;
+	uint64_t delayUntilNextCMD;
+	uint64_t timeOfNextCMD;
+
+	OnboardTC tc;
+	OnboardTC *nextCmd;
+
+	printf("%lu\tID:0x%X\ttask started\n", gettime(sec), sParameters->taskID);
+
+	int i = 0;
+	while (1) {
+		printf("%lu\tID:0x%X\ttask running\n", gettime(sec), sParameters->taskID);
+
+		if (prioq->len > 0) {
+
+			nextCmd = top(prioq);
+			timeOfNextCMD = (uint64_t) nextCmd->time * 1000; //convert from sec to ms
+
+			timeValSec = gettime(sec);
+			timeVal = timeValSec * 1000; //convert from sec to ms
+
+			if (timeOfNextCMD > timeVal)
+				delayUntilNextCMD = timeOfNextCMD - timeVal;
+			else
+				delayUntilNextCMD = 0;
+
+
+			if (sParameters->period < delayUntilNextCMD ) {
+				sdelay(sParameters->period, ms);
+			}
+			else {
+				sdelay(delayUntilNextCMD, ms);
+				pop(prioq, &tc);
+
+				if (tc.taskID == SendTMTaskID) {
+					cwrite(sendTMQueue, tc);
+				}
+				else {
+					for (i = 0; i < numberOfPayloadQueues; i++) {
+						if (payloadQueues[i].taskID == tc.taskID)
+							break;
+					}
+					if (i == numberOfPayloadQueues)
+						printf("can't find queue for %u task\n", sParameters->taskID);
+					else
+						cwrite(payloadQueues[i].queueHandle, tc);
+
+				}
+			}
+		}
+		else
+			sdelay(sParameters->period, ms);
+	}
+
+}
+
 task SendTM(void * vParameters) {
 	SendTMParameters * sParameters  = (SendTMParameters *) vParameters;
 
-	uint16_t error = 0;
+	int16_t error = 0;
 
 	uint8_t moreData = 0;
 
@@ -253,30 +232,32 @@ task SendTM(void * vParameters) {
 	encodedDataToSend.dataField.size = MAX_DATA_SIZE_SENDTM;
 
 
-	printf("%lu\tID:0x%X\ttask started\n", gettime(ms), sParameters->taskID);
+	printf("%lu\tID:0x%X\ttask started\n", gettime(sec), sParameters->taskID);
 
 	while (1) {
-		printf("%lu\tID:0x%X\ttask running\n", gettime(ms), sParameters->taskID);
+		printf("%lu\tID:0x%X\ttask running\n", gettime(sec), sParameters->taskID);
 
-		error = cread(sendTMQueue, receivedTC);
-		error ? (error = 0) : (error = 1); //invert error, because it returns 1 on success and 0 on error
+		cread(sendTMQueue, receivedTC);
 		i = 0;
 		do {
-			printf("%lu\tID:0x%X\ttask sending TM %d\n", gettime(ms), sParameters->taskID, i);
 			if (!error) error = sParameters->readMemory(&receivedTC, &dataToSend, &moreData);
 
 			if (!error) error = sParameters->encodeTM(&receivedTC, &dataToSend, &encodedDataToSend);
 
 			if (!error) error = sParameters->sendOnboard(&encodedDataToSend);
 
+			if (!error)	printf("%lu\tID:0x%X\ttask TM %d sent\n",
+				                   gettime(sec),
+				                   sParameters->taskID, i);
+
 			i++;
 
-		} while (moreData && !error);
+		} while (moreData && !error && nelem(sendTMQueue) == 0);
 		moreData = 0;
+		error = 0;
 	}
 
 }
-
 
 task CollectHK(void * vParameters) {
 
@@ -284,7 +265,7 @@ task CollectHK(void * vParameters) {
 
 	spriority(sParameters->priority);
 
-	uint16_t error = 0;
+	int16_t error = 0;
 
 	OnboardTM receivedData;
 	uint8_t receivedDataBuffer[MAX_DATA_SIZE_COLLECTHK];
@@ -295,10 +276,10 @@ task CollectHK(void * vParameters) {
 
 	sParameters->initialization();
 
-	printf("%lu\tID:0x%X\ttask started\n", gettime(ms), sParameters->taskID);
+	printf("%lu\tID:0x%X\ttask started\n", gettime(sec), sParameters->taskID);
 	while (1) {
 
-		printf("%lu\tID:0x%X\ttask running\n", gettime(ms), sParameters->taskID);
+		printf("%lu\tID:0x%X\ttask running\n", gettime(sec), sParameters->taskID);
 
 		for (i = 0; i < sParameters->numberOfReceiveOnboards; i++) {
 
@@ -306,12 +287,53 @@ task CollectHK(void * vParameters) {
 
 			if (!error) error = sParameters->writeMemory(&receivedData);
 
-			if (error) printf("error in %d. collectHK readMemory %u\n", i, error);
+			if (error) printf("error in %d. collectHK readMemory %d\n", i, error);
 
 		}
 
 		sdelay(sParameters->period, ms);
 
+	}
+
+}
+
+task Subsystem( void * vParameters) {
+
+	SubsystemParameters * sParameters = (SubsystemParameters *) vParameters;
+
+	spriority(sParameters->priority);
+
+	DynamicData dataFromSensors;
+	uint8_t dataFromSensorsBuffer[MAX_DATA_SIZE_SUBSYSTEM];
+	dataFromSensors.data = dataFromSensorsBuffer;
+	dataFromSensors.size = MAX_DATA_SIZE_SUBSYSTEM;
+
+	DynamicData dataToActuators;
+	uint8_t dataToActuatorsBuffer[MAX_DATA_SIZE_SUBSYSTEM];
+	dataToActuators.data = dataToActuatorsBuffer;
+	dataToActuators.size = MAX_DATA_SIZE_SUBSYSTEM;
+
+	int16_t error = 0;
+
+	sParameters->initialization();
+
+	printf("%lu\tID:0x%X\ttask started\n", gettime(sec), sParameters->taskID);
+
+	while (1) {
+		printf("%lu\tID:0x%X\ttask running\n", gettime(sec), sParameters->taskID);
+
+		error = 0;
+
+		//read values from magnetometers
+		error = sParameters->receiveOnboard(&dataFromSensors);
+
+		//calculate
+		if (!error) error = sParameters->calculation(&dataFromSensors, &dataToActuators);
+
+		//send back results to subsystem
+		if (!error) error = sParameters->sendOnboard(&dataToActuators);
+
+		sdelay(sParameters->period, ms);
 	}
 
 }
@@ -325,14 +347,14 @@ task ControlPL(void * vParameters) {
 
 	spriority(sParameters->priority);
 
-	uint16_t error = 0;
+	int16_t error = 0;
 
 	int i = 0;
-	for (i = 0; i < NumberOfPayloadQueues; i++) {
+	for (i = 0; i < numberOfPayloadQueues; i++) {
 		if (payloadQueues[i].taskID == sParameters->taskID)
 			break;
 	}
-	if (i==NumberOfPayloadQueues) printf("can't find queue for %u task\n",sParameters->taskID);
+	if (i == numberOfPayloadQueues) printf("can't find queue for %u task\n", sParameters->taskID);
 
 	OnboardTC receivedTC;
 	receivedTC.size = MAX_DATA_SIZE_TC;
@@ -342,26 +364,24 @@ task ControlPL(void * vParameters) {
 	PLData.dataField.data = PLDataBuffer;
 	PLData.dataField.size = MAX_DATA_SIZE_PAYLOAD_POLL;
 
-	printf("%lu\tID:0x%X\ttask started\n", gettime(ms), sParameters->taskID);
+	printf("%lu\tID:0x%X\ttask started\n", gettime(sec), sParameters->taskID);
 
 	while (1) {
-		printf("%lu\tID:0x%X\ttask ready to start\n", gettime(ms), sParameters->taskID);
+		printf("%lu\tID:0x%X\ttask ready to start\n", gettime(sec), sParameters->taskID);
 
-		error = cread(payloadQueues[i].queueHandle, receivedTC);
-		error ? (error = 0) : (error = 1); //invert error, because it returns 1 on success and 0 on error
+		cread(payloadQueues[i].queueHandle, receivedTC);
 		error = sParameters->startPL(&receivedTC);
 
 		while (!error) {
-			printf("%lu\tID:0x%X\ttask ready for commands\n", gettime(ms), sParameters->taskID);
+			printf("%lu\tID:0x%X\ttask ready for commands\n", gettime(sec), sParameters->taskID);
 
-			error = cread(payloadQueues[i].queueHandle, receivedTC);
-			error ? (error = 0) : (error = 1); //invert error, because it returns 1 on success and 0 on error
+			cread(payloadQueues[i].queueHandle, receivedTC);
 			error = sParameters->checkTC(&receivedTC, &numberOfPolls, &pollInterval);
 
 			if (!error) {
 				int j = 0;
 				while (nelem(payloadQueues[i].queueHandle) == 0  && j < numberOfPolls) {
-					printf("%lu\tID:0x%X\ttask running %d/%d\n", gettime(ms), sParameters->taskID, j + 1, numberOfPolls);
+					printf("%lu\tID:0x%X\ttask running %d/%d\n", gettime(sec), sParameters->taskID, j + 1, numberOfPolls);
 					PLData.dataField.size = MAX_DATA_SIZE_PAYLOAD_POLL;
 					error = sParameters->receiveOnboard(&receivedTC, &PLData);
 
@@ -373,12 +393,13 @@ task ControlPL(void * vParameters) {
 			}
 			else {
 				sParameters->stopPL(&receivedTC);
-				printf("%lu\tID:0x%X\ttask stopped\n", gettime(ms), sParameters->taskID);
+				printf("%lu\tID:0x%X\ttask stopped\n", gettime(sec), sParameters->taskID);
 
 			}
 		}
 
 	}
+
 }
 
 
--- framework.h
+++ framework.h
@@ -7,8 +7,9 @@
 #include "priorityQueue_v3.h"		//prioq implementation
 #include "frameworkDataStructs.h"
 
-#define MAX_NUMBER_OF_TASKS 	20
-#define MAX_NUMBER_OF_PAYLOADS 	10
+#define MAX_NUMBER_OF_TASKS 		20
+#define MAX_NUMBER_OF_SUBSYSTEMS	5
+#define MAX_NUMBER_OF_PAYLOADS 		10
 
 #define MAX_DATA_SIZE_SUBSYSTEM		100
 #define MAX_DATA_SIZE_COLLECTHK		100
@@ -18,95 +19,111 @@
 #define FIFOCHANNEL_MAXDELAY portMAX_DELAY
 #define EMPTY_FIFO_CHANNEL pdFALSE
 
+uint8_t numberOfTasks;
+
+
+//taskIDs of core tasks
+uint8_t GetTCTaskID;
+uint8_t SendTMTaskID;
+
+
+
 //parameters for Initialization task
-typedef struct _InitializationParameters{
+typedef struct _InitializationParameters {
 	uint16_t taskID;
 	uint16_t priority;
 	uint8_t numberOfTasks;
 	TaskDefinition * taskDefinitions;
-	uint16_t (*initialization)();
-}InitializationParameters;
+	int16_t (*initialization)();
+} InitializationParameters;
 
-typedef struct _HandleCMDParameters{
-	uint16_t taskID;
-	uint16_t priority;
-	uint16_t period;
-}HandleCMDParameters;
-
-//parameters for ADCS and EPS tasks
-typedef struct _SubsystemParameters{
-	uint16_t taskID;
-	uint16_t priority;
-	uint16_t period;
-	uint16_t (*initialization)();
-	uint16_t (*receiveOnboard)(DynamicData * dataFromSensors);
-	uint16_t (*calculation)(DynamicData * dataFromSensors, DynamicData * dataToActuators);
-	uint16_t (*sendOnboard)(DynamicData * dataToActuators);
-}SubsystemParameters;
 
 
 //parameters for GetTC task
-typedef struct _GetTCParameters{
+typedef struct _GetTCParameters {
 	uint16_t taskID;
 	uint16_t priority;
 	uint16_t period;
-	uint16_t (*receiveOnboard)(DynamicData * dataFromCS);
-	uint16_t (*decodeTC)(DynamicData * dataFromCS, OnboardTC * receivedTC);
-}GetTCParameters;
+	int16_t (*receiveOnboard)(DynamicData * dataFromCS);
+	int16_t (*decodeTC)(DynamicData * dataFromCS, OnboardTC * receivedTC);
+} GetTCParameters;
+
+
+
+//parameters for HandleCMD task
+typedef struct _HandleCMDParameters {
+	uint16_t taskID;
+	uint16_t priority;
+	uint16_t period;
+} HandleCMDParameters;
+
 
 
 //parameters for SendTM task
-
-typedef struct _SendTMParameters{
+typedef struct _SendTMParameters {
 	uint16_t taskID;
 	uint16_t priority;
-	uint16_t (*readMemory)(OnboardTC * receivedTC, OnboardTM * dataToSend, uint8_t * moreData);
-	uint16_t (*encodeTM)(OnboardTC * receivedTC, OnboardTM * dataToSend, OnboardTM * encodedDataToSend);
-	uint16_t (*sendOnboard)(OnboardTM * encodedDataToSend);
-}SendTMParameters;
+	int16_t (*readMemory)(OnboardTC * receivedTC, OnboardTM * dataToSend, uint8_t * moreData);
+	int16_t (*encodeTM)(OnboardTC * receivedTC, OnboardTM * dataToSend, OnboardTM * encodedDataToSend);
+	int16_t (*sendOnboard)(OnboardTM * encodedDataToSend);
+} SendTMParameters;
+
 
 
 //parameters for CollectHK task
-typedef struct _ReceiveOnboard{
+typedef struct _ReceiveOnboard {
 	uint8_t taskID;
-	uint16_t (*function)(OnboardTM * receivedData);	
-}ReceiveOnboard;
+	int16_t (*function)(OnboardTM * receivedData);
+} ReceiveOnboard;
 
-typedef struct _CollectHKParameters{
+typedef struct _CollectHKParameters {
 	uint16_t taskID;
 	uint16_t priority;
 	uint16_t period;
-	uint16_t (*initialization)();
+	int16_t (*initialization)();
 	ReceiveOnboard * receiveOnboards;
 	uint8_t numberOfReceiveOnboards;
-	uint16_t (*writeMemory) (OnboardTM * receivedData);	
-}CollectHKParameters;
+	int16_t (*writeMemory) (OnboardTM * receivedData);
+} CollectHKParameters;
 
-//parameters for ControlPL task
-typedef struct _ControlPLParameters{
+
+
+//parameters for Subsystem tasks
+typedef struct _SubsystemParameters {
 	uint16_t taskID;
 	uint16_t priority;
-	uint16_t (*startPL)(OnboardTC * receivedTC);
-	uint16_t (*checkTC)(OnboardTC * receivedTC, uint16_t * numberOfPolls, uint32_t * pollInterval);
-	uint16_t (*receiveOnboard)(OnboardTC * receivedTC, OnboardTM * PLData);
-	uint16_t (*writeMemory)(OnboardTC * receivedTC, OnboardTM * PLData);
-	uint16_t (*stopPL)(OnboardTC * receivedTC);
-}ControlPLParameters;
+	uint16_t period;
+	int16_t (*initialization)();
+	int16_t (*receiveOnboard)(DynamicData * dataFromSensors);
+	int16_t (*calculation)(DynamicData * dataFromSensors, DynamicData * dataToActuators);
+	int16_t (*sendOnboard)(DynamicData * dataToActuators);
+} SubsystemParameters;
 
-typedef struct _FrameworkQueue{
+//parameters for ControlPL task
+typedef struct _ControlPLParameters {
+	uint16_t taskID;
+	uint16_t priority;
+	int16_t (*startPL)(OnboardTC * receivedTC);
+	int16_t (*checkTC)(OnboardTC * receivedTC, uint16_t * numberOfPolls, uint32_t * pollInterval);
+	int16_t (*receiveOnboard)(OnboardTC * receivedTC, OnboardTM * PLData);
+	int16_t (*writeMemory)(OnboardTC * receivedTC, OnboardTM * PLData);
+	int16_t (*stopPL)(OnboardTC * receivedTC);
+} ControlPLParameters;
+
+
+
+//struct for queue handling
+typedef struct _FrameworkQueue {
 	uint8_t taskID;
-	xQueueHandle queueHandle;	
-}FrameworkQueue;
+	xQueueHandle queueHandle;  //TODO
+} FrameworkQueue;
 
-uint8_t numberOfTasks;
 
-uint8_t GetTCTaskID;
-uint8_t SendTMTaskID;
 
 //queues
 heap_t* prioq;
 
 FrameworkQueue payloadQueues[MAX_NUMBER_OF_PAYLOADS];
-uint8_t NumberOfPayloadQueues;
+uint8_t numberOfPayloadQueues;
 
 #endif
